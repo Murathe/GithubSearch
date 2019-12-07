951dc3ee3dcb47880128015c265685e9e8afdfeb
murathe <isaacsm07p@gmail.com>
2019-12-07 22:39:10 +0300

    Add search functions

--- src/app/accont-search/accont-search.component.ts
+++ src/app/accont-search/accont-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubSearchService } from '../github-search.service';
 
 @Component({
   selector: 'app-accont-search',
@@ -10,7 +11,21 @@ export class AccontSearchComponent implements OnInit {
   username: string;
   user: string[];
   repos: string[];
-  constructor() { }
+
+  constructor(private githubsearchService: GithubSearchService) { 
+    this.githubsearchService.findUser().subscribe(user => {
+      console.log(user);
+      this.user = user;
+    });
+    this.githubsearchService.findRepos().subscribe(repo =>{
+      console.log(repo);
+      this.repos = repo;
+    });
+  }
+
+  searchUser(){
+    this.githubsearchService.replaceUser(this.username);
+  }
 
   ngOnInit() {
   }
