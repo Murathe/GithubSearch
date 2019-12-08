99ef8dce5b9accb56f4defddc35e9cda67c5c0c6
murathe <isaacsm07p@gmail.com>
2019-12-08 10:48:02 +0300

    Call the findUser/REpo functions

--- src/app/github-search.service.ts
+++ src/app/github-search.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubSearchService {
 
-  constructor() { }
+  apiUrl = environment.apiUrl;
+  private username: string;
+  private repo: string;
+
+  constructor(private http: HttpClient) { 
+    console.log("ready");
+    this.username = "Murathe";
+    this.repo = '';
+  }
+  findUser(): any {
+    return this.http.get("http://api.github.com/users/" + this.username + "?access_token=b7e19011dae2fe6736b3454bb6f56637774ae721").pipe((res => res));
+  }
+  findRepos(): any{
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?access_token=b7e19011dae2fe6736b3454bb6f56637774ae721").pipe((res => res));
+  }
+  
+
+
 }
